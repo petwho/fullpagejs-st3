e0a6943ff45b0c74b0494bd8382e7d47739c4b7c
Tran Khanh <khanhth.evolableasia@gmail.com>
2016-07-30 19:21:24 +0700

    Hotfix

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>
 
-	<script type="text/javascript" src="/js/jquery.fullPage.js"></script>
+	<script type="text/javascript" src="/js/jquery.fullpage.js"></script>
 	<!-- <script type="text/javascript" src="/js/examples.js"></script> -->
 
 	<script type="text/javascript">
