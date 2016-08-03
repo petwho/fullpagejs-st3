644ff48a102dc12d4acb9540317aa5e117a630a8
Tran Khanh <khanhth.evolableasia@gmail.com>
2016-08-03 22:12:49 +0700

    Hotfix

--- index_vi.php
+++ index_vi.php
@@ -2,9 +2,9 @@
 	<div class="section" id="section-home">
 		<div class="container">
 			<div class="fix lang">
-				<a href="/index.php?lang=vi"<?php echo @$_GET['lang'] == 'vi' ? ' class="active"' : '' ?>VN</a>
+				<a href="/index.php?lang=vi"<?php echo @$_GET['lang'] == 'vi' ? ' class="active"' : '' ?>>VN</a>
 				/
-				<a href="/" <?php echo !isset($_GET['lang']) ? ' class="active"' : '' ?>EN</a>
+				<a href="/" <?php echo !isset($_GET['lang']) ? ' class="active"' : '' ?>>EN</a>
 			</div>
 		</div>
 		<div class="scroller row">
@@ -130,8 +130,8 @@ We enhance our customers living experience with the beauty of accessories; we ad
 								2A Nguyễn Thiệp, Quận 1, Thành phố Hồ Chí Minh, Việt Nam
 							</address>
 							<h3>FOLLOW US</h3>
-							<div><a href="http://facebook.com/smallerthanthreevn.vn">facebook.com/smallerthanthree.vn</a></div>
-							<div><a href="http://instagram.com/smallerthanthreevn.vn">instagram.com/smallerthanthree.vn</a></div>
+							<div><a class="link" href="http://facebook.com/smallerthanthreevn.vn">facebook.com/smallerthanthree.vn</a></div>
+							<div><a class="link" href="http://instagram.com/smallerthanthreevn.vn">instagram.com/smallerthanthree.vn</a></div>
 						</div>
 					</div>
 				</div>
