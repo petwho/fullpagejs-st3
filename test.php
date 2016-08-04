3ae5cd7a01ed6e586f6af9866aaec5b916fa9853
Tran Khanh <khanhth.evolableasia@gmail.com>
2016-08-05 00:38:45 +0700

    Hotfix

---
+++ test.php
@@ -0,0 +1,127 @@
+<?php
+include('./form.php');
+function assetSrc($file) {
+	return $file.'?'.filemtime(__DIR__.'/'.$file);
+}
+$lang = @$_GET['lang'];
+$langQuerySuff = ($lang == 'vi') ? '&lang=vi' : '';
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Smaller Than Three</title>
+	<!-- Bootstrap -->
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/nanoscroller.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <!-- <link rel="stylesheet" href="/css/animate.css"> -->
+	<link rel="stylesheet" type="text/css" href="<?php echo assetSrc('/css/jquery.fullpage.css'); ?>" />
+	<link rel="stylesheet" type="text/css" href="<?php echo assetSrc('/css/main.css'); ?>" />
+
+	<!-- <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script> -->
+	<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> -->
+	<!-- <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script> -->
+
+	<script src="/js/jquery.min.js"></script>
+	<script type="text/javascript" src="/js/jquery.fullpage.js"></script>
+	<script type="text/javascript">
+		// var disUpId, disDownId, $fullpage, nanoClass;
+		$(document).ready(function() {
+			$('form').submit(function (e) {
+				var $button = $(this).find('button');
+				var $name = $(this).find('input[name="name"]');
+				var $email = $(this).find('input[name="email"]');
+				var $content = $(this).find('textarea[name="content"]');
+
+				$button.prop('disabled', true);
+				$.ajax({
+					method: 'POST',
+					url: '/form.php',
+					data: {
+						name: $name.val(),
+						email: $email.val(),
+						content: $content.val(),
+					}
+				}).success(function () {
+					$('.alert.hidden').removeClass('hidden');
+					$name.val('');
+					$email.val('');
+					$content.val('');
+					$button.prop('disabled', false);
+				});
+				e.preventDefault();
+			});
+
+			$('#fullpage').fullpage({
+				verticalCentered: false,
+
+				//to avoid problems with css3 transforms and fixed elements in Chrome, as detailed here: https://github.com/alvarotrigo/fullPage.js/issues/208
+				css3: true,
+				navigation: true,
+				navigationPosition: 'right',
+				navigationTooltips: ['home', 'about', 'lá collection', 'details', 'contact'],
+				normalScrollElements: '.nano',
+		        onLeave: function(index, nextIndext) {
+		            if(nextIndext == 1 || nextIndext == 3 || nextIndext == 4){
+		                setTimeout(function () {
+			                $('#fp-nav').addClass('white');
+		                }, 300);
+		            } else {
+			            setTimeout(function () {
+				            $('#fp-nav').removeClass('white');
+			            }, 300);
+		            }
+		            if (!$('#check-visible-xs').is(':visible')) {
+		            	$('.animate').css({top: 'auto', opacity: 0});
+		            }
+		        },
+		        afterLoad: function(anchorLink, index){
+	                var $loadedSection = $(this);
+	                if (!$('#check-visible-xs').is(':visible')) {
+		                $loadedSection.find('.animate').css({
+		                	position: 'relative',
+		                	opacity: 0,
+		                }).animate({
+		                	opacity: 1,
+		                	top: '15px',
+		                }, 300);
+	                }
+	            }
+			});
+
+			$('#fp-nav').addClass('white');
+
+			$('.container-artwork img').click(function (argument) {
+				location.href = '/artwork.php?id=' + $(this).data('page') + "<?php echo $langQuerySuff; ?>";
+			});
+		});
+	</script>
+
+</head>
+<body>
+<header>
+	<!-- <a href="/"><span class="separator">smaller</span><span class="separator">than</span><span class="separator">three</span></a> -->
+	<a href="/"><img class="logo" src="/img/logo.png" alt="Smaller Than Three Logo"></a>
+</header>
+<?php if ($lang == 'vi'): ?>
+	<?php include('test_vi.php'); ?>
+<?php else: ?>
+	<?php include('test_en.php'); ?>
+<?php endif; ?>
+<div id="check-visible-xs" class="visible-xs"></div>
+<div class="hidden">
+	<img src="/img/bg_art_md.jpg" alt="">
+	<img src="/img/bg_art_xs.jpg" alt="">
+</div>
+</body>
+</html>
---
+++ test_en.php
@@ -0,0 +1,96 @@
+<div id="fullpage">
+	<div class="section section-index-1" id="section-home">
+		<div class="container">
+			<div class="fix lang">
+				<a href="/index.php?lang=vi"<?php echo @$_GET['lang'] == 'vi' ? ' class="active"' : '' ?>>VN</a>
+				/
+				<a href="/" <?php echo !isset($_GET['lang']) ? ' class="active"' : '' ?>>EN</a>
+			</div>
+		</div>
+		<div class="scroller row">
+			<div class="col-xs-12">
+				<div class="text-center">
+					<p><strong>scroll to begin</strong></p>
+					<p>
+						<!-- <img class="icon" src="/img/down-arrow.jpg" width="auto" height="30" alt="scroller"> -->
+						<span class="icon"></span>
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="section section-index-2" id="section-about">
+		<div class="container nano disable-ip6">
+			<div class="row nano-content">
+				<div class="animate">
+					<div class="col-xs-12 col-md-6">
+						<h3>ABOUT SMALLER THAN THREE</h3>
+						<p><strong>Smaller Than Three</strong> is a concept brand introduced by Tim Pham.<br>
+	Being one of the first few concept stores in Vietnam, we help our customers to celebrate the aesthetics of everyday living, starting with our surrounding items. We focus on the modernity, classiness and simplicity of our product lines, which delivers high quality standard with an inspirational imprint of Vietnam. <br>
+	We enhance our customers living experience with the beauty of accessories; we add a little love into everything you have. 
+	</p>
+					</div>
+					<div class="col-hack col-md-1 visible-md visible-lg visible-sm"></div>
+					<div class="col-xs-12 col-md-6">
+						<h3>LÁ COLLECTION</h3>
+						<p>"Leaves have great impacts on our lives in many ways. They fuel plant life, offer shelter to small wildlife and create a living environment for microorganism. Personally, the color green has raised my soul with unforgettable memories. My subject matter is the fresh color green –from papaya, banana, sacred fig, sponge gourd, flame tree, shiso and aloe vera leaf – a truly inspiring essence of natural beauty. As I was painting these leaves, I was on a journey revisiting the stories that lie within each leaf.  Our little front yard, my grandmother’s smile, my mother’s hands, the first drop of summer rain, and the late autumn sunray captured through the spirit of the fresh color green are what keeps my dreams forever intact." - Tim Pham</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php include('./artworks.php'); ?>
+	<div class="section section-index-4" id="section-details">
+		<div class="container">
+			<div class="row">
+				<div class="animate">
+					<div class="col-xs-12 col-md-6">
+						<h3>DIMENSIONS</h3>
+						<p>
+							<strong>Calendar</strong>
+							<br>
+							<span>20 cm x 20 cm</span>
+						</p>
+						<p>
+							<strong>Sketchbook</strong>
+							<br>
+							<span>14.8 cm x 22.3 cm</span>
+						</p>
+						<p>
+							<strong>Postcard</strong>
+							<br>
+							<span>12 cm x 17 cm</span>
+						</p>
+						<p>
+							<strong>Frames</strong>
+							<br>
+							50 cm x 70 cm<br>
+							40 cm x 60 cm<br>
+							16 cm x 22 cm
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="section section-index-5" id="section-contact">
+		<div class="container nano">
+			<div class="row nano-content">
+							<div class="animate">
+								<div class="col-xs-12 col-md-6">
+									<h3>ABOUT SMALLER THAN THREE</h3>
+									<p><strong>Smaller Than Three</strong> is a concept brand introduced by Tim Pham.<br>
+				Being one of the first few concept stores in Vietnam, we help our customers to celebrate the aesthetics of everyday living, starting with our surrounding items. We focus on the modernity, classiness and simplicity of our product lines, which delivers high quality standard with an inspirational imprint of Vietnam. <br>
+				We enhance our customers living experience with the beauty of accessories; we add a little love into everything you have. 
+				</p>
+								</div>
+								<div class="col-hack col-md-1 visible-md visible-lg visible-sm"></div>
+								<div class="col-xs-12 col-md-6">
+									<h3>LÁ COLLECTION</h3>
+									<p>"Leaves have great impacts on our lives in many ways. They fuel plant life, offer shelter to small wildlife and create a living environment for microorganism. Personally, the color green has raised my soul with unforgettable memories. My subject matter is the fresh color green –from papaya, banana, sacred fig, sponge gourd, flame tree, shiso and aloe vera leaf – a truly inspiring essence of natural beauty. As I was painting these leaves, I was on a journey revisiting the stories that lie within each leaf.  Our little front yard, my grandmother’s smile, my mother’s hands, the first drop of summer rain, and the late autumn sunray captured through the spirit of the fresh color green are what keeps my dreams forever intact." - Tim Pham</p>
+								</div>
+							</div>
+			</div>
+		</div>
+	</div>
+</div>
