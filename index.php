1ca8f249f924ec49d193a0ae6962c6606017d62a
Tran Khanh <khanhth.evolableasia@gmail.com>
2016-08-04 23:24:18 +0700

    Hotfix

--- index.php
+++ index.php
@@ -17,7 +17,6 @@ $langQuerySuff = ($lang == 'vi') ? '&lang=vi' : '';
 	<!-- Bootstrap -->
     <link rel="stylesheet" href="/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/nanoscroller.css">
-    <!-- <link rel="stylesheet" href="/css/font-awesome.min.css"> -->
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -29,20 +28,12 @@ $langQuerySuff = ($lang == 'vi') ? '&lang=vi' : '';
 	<link rel="stylesheet" type="text/css" href="<?php echo assetSrc('/css/jquery.fullpage.css'); ?>" />
 	<link rel="stylesheet" type="text/css" href="<?php echo assetSrc('/css/main.css'); ?>" />
 
-	<!--[if IE]>
-		<script type="text/javascript">
-			 var console = { log: function() {} };
-		</script>
-	<![endif]-->
-
 	<!-- <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script> -->
 	<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> -->
 	<!-- <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script> -->
 
 	<script src="/js/jquery.min.js"></script>
-    <!-- <script src="/js/jquery.easings.min.js"></script> -->
 	<script type="text/javascript" src="/js/jquery.fullpage.js"></script>
-	<!-- <script src="/js/jquery.nanoscroller.min.js"></script> -->
 	<script type="text/javascript">
 		var disUpId, disDownId, $fullpage, nanoClass;
 		$(document).ready(function() {
@@ -70,15 +61,7 @@ $langQuerySuff = ($lang == 'vi') ? '&lang=vi' : '';
 				});
 				e.preventDefault();
 			});
-			// if ($('#check-sm-screen').is(':visible')) {
-			// 	if ($('#check-ip6').is(':visible')) {
-		 //            nanoClass = '.nano:not(".disable-ip6")';
-			// 	} else {
-		 //            nanoClass = '.nano';
-			// 	}
-	  //       }
-	        // $(nanoClass).nanoScroller();
-	        // $(nanoClass).find('.nano-content').css({paddingBottom: '15px'});
+
 			$('#fullpage').fullpage({
 				verticalCentered: false,
 
@@ -89,79 +72,29 @@ $langQuerySuff = ($lang == 'vi') ? '&lang=vi' : '';
 				navigationTooltips: ['home', 'about', 'lá collection', 'details', 'contact'],
 				normalScrollElements: '.nano',
 		        onLeave: function(index, nextIndext) {
-		        	// $(nanoClass).find('.nano-content').css({overflow: 'hidden'});
-		        	// $(nanoClass).find('.nano-content').css({overflowY: 'scroll'});
-		        	// $(nanoClass).nanoScroller();
 		            if(nextIndext == 1 || nextIndext == 3 || nextIndext == 4){
-		                // $('#fp-nav, header').addClass('white');
 		                setTimeout(function () {
 			                $('#fp-nav').addClass('white');
 		                }, 300);
 		            } else {
-			            // $('#fp-nav, header').removeClass('white');
 			            setTimeout(function () {
 				            $('#fp-nav').removeClass('white');
 			            }, 300);
 		            }
 
 	            	$('.section').find('p, h3, address, form, a.link, .alert').css({top: 'auto', opacity: 0});
-            		// if ($('#check-sm-screen').is(':visible')) {
-	    	        // 	if (nextIndext == 2 || nextIndext == 5) {
-	    		       //  	$(nanoClass).bind("scrollend", function(e){
-	    		       //  		console.log('scrollend')
-	    		       //  		disUpId = setTimeout(function () {
-	    		       //  	        $fullpage.setAllowScrolling(false, 'up');
-	    		       //  	        $fullpage.setAllowScrolling(true, 'down');
-	    		       //  		}, 800);
-	    		       //  	});
-
-	    		       //  	$(nanoClass).bind("scrolltop", function(e){
-	    		       //  		console.log('scrolltop')
-	    		       //  		disDownId = setTimeout(function () {
-	    		       //  	        $fullpage.setAllowScrolling(false, 'down');
-	    		       //  	        $fullpage.setAllowScrolling(true, 'up');
-	    		       //  		}, 800);
-	    		       //  	});
-	    	        // 	}
-			           //  if ((nextIndext == 2) && !$('#check-ip6').is(':visible')){
-			           //  	if (index == 1) {
-				          //       $fullpage.setAllowScrolling(true, 'up');
-				          //       $fullpage.setAllowScrolling(false, 'down');
-			           //  	} else if (index == 3) {
-				          //   	$fullpage.setAllowScrolling(true, 'down');
-				          //   	$fullpage.setAllowScrolling(false, 'up');
-				          //   }
-			           //  } else if (nextIndext == 5){
-	             //        	if (index == 4) {
-	        	    //             $fullpage.setAllowScrolling(true, 'up');
-	        	    //             $fullpage.setAllowScrolling(false, 'down');
-	             //        	}
-	             //        } else {
-	             //        	$(nanoClass).unbind("scrollend");
-	             //        	$(nanoClass).unbind("scrolltop");
-	             //        	clearTimeout(disUpId);
-	             //        	clearTimeout(disDownId);
-	             //        	console.log('enable up down');
-	             //        	if ($fullpage) {
-			           //  		$fullpage.setAllowScrolling(true, 'up');
-			           //  		$fullpage.setAllowScrolling(true, 'down');
-	             //        	}
-	    	        //     }
-    	         //    }
 		        },
 		        afterLoad: function(anchorLink, index){
 	                var $loadedSection = $(this);
-	              //   if ((index != 2) && (index != 5) && $fullpage) {
-	            		// $fullpage.setAllowScrolling(true);
-	              //   }
-	                $loadedSection.find('p, h3, address, form, a.link, .alert').css({'position': 'relative', 'opacity': 0}).animate({
+	                $loadedSection.find('p, h3, address, form, a.link, .alert').css({
+	                	'position': 'relative',
+	                	'opacity': 0
+	                }).animate({
 	                	opacity: 1,
 	                	top: '15px',
 	                }, 300);
 	            }
 			});
-
-			// $fullpage = $.fn.fullpage;
 
 			$('#fp-nav').addClass('white');
 
