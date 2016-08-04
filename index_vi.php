b018c62a713765f32f821913e66c914d076f1733
Tran Khanh <khanhth.evolableasia@gmail.com>
2016-08-04 23:35:16 +0700

    Hotfix

--- index_vi.php
+++ index_vi.php
@@ -22,18 +22,19 @@
 	<div class="section" id="section-about">
 		<div class="container nano">
 			<div class="row nano-content">
-				<div class="col-xs-12 col-md-6">
-					<h3>ABOUT SMALLER THEN THREE</h3>
-					<p><strong>Smaller Than Three</strong> is a concept brand introduced by Tim Pham.<br>
-Being one of the first few concept stores in Vietnam, we help our customers to celebrate the aesthetics of everyday living, starting with our surrounding items. We focus on the modernity, classiness and simplicity of our product lines, which delivers high quality standard with an inspirational imprint of Vietnam. <br>
-We enhance our customers living experience with the beauty of accessories; we add a little love into everything you have. 
-</p>
-				</div>
-				<div class="col-hack col-md-1 visible-md visible-lg visible-sm"></div>
-				<div class="col-xs-12 col-md-6">
-					<h3>BỘ SƯU TẬP LÁ</h3>
-					<p>"Lá với tôi không chỉ là cơ quan sinh dưỡng của cây mà màu xanh của nó đã nuôi nấng tâm hồn tôi bằng những kỷ niệm… Tôi vẽ lá đu đủ, lá chuối, lá bồ đề, lá mướp, lá phượng, lá tía tô và nha đam không đơn giản bởi vẻ đẹp tự nhiên của nó mà ở mỗi chiếc lá còn cất giữ trong tôi những câu chuyện đẹp thuở ấu thơ… Mảnh sân nhỏ trước nhà, nụ cười của bà, đôi bàn tay của mẹ, những giọt mưa mùa hạ, những tia nắng cuối thu… Qua kẽ lá, những giấc mơ trong tôi còn nguyên mãi…" - Tim Phạm</p>
-					<!-- <p>Lá với tôi không chỉ là cơ quan sinh dưỡng của cây mà màu xanh của nó đã nuôi nấng tâm hồn tôi bằng những kỷ niệm… tôi vẽ lá đu đủ, lá chuối, lá bồ đề, lá mướp, lá phượng, lá tía tô và nha đam không đơn giản bởi vẻ đẹp tự nhiên của nó mà ở mỗi chiếc lá còn cất giữ trong tôi những câu chuyện đẹp thuở ấu thơ… mảnh sân nhỏ trước nhà, nụ cười của bà, đôi bàn tay của mẹ, những giọt mưa mùa hạ, những tia nắng cuối thu… qua kẽ lá, những giấc mơ trong tôi còn nguyên mãi…</p> -->
+				<div class="animate">
+					<div class="col-xs-12 col-md-6">
+						<h3>ABOUT SMALLER THEN THREE</h3>
+						<p><strong>Smaller Than Three</strong> is a concept brand introduced by Tim Pham.<br>
+						Being one of the first few concept stores in Vietnam, we help our customers to celebrate the aesthetics of everyday living, starting with our surrounding items. We focus on the modernity, classiness and simplicity of our product lines, which delivers high quality standard with an inspirational imprint of Vietnam. <br>
+						We enhance our customers living experience with the beauty of accessories; we add a little love into everything you have. 
+						</p>
+					</div>
+					<div class="col-hack col-md-1 visible-md visible-lg visible-sm"></div>
+					<div class="col-xs-12 col-md-6">
+						<h3>BỘ SƯU TẬP LÁ</h3>
+						<p>"Lá với tôi không chỉ là cơ quan sinh dưỡng của cây mà màu xanh của nó đã nuôi nấng tâm hồn tôi bằng những kỷ niệm… Tôi vẽ lá đu đủ, lá chuối, lá bồ đề, lá mướp, lá phượng, lá tía tô và nha đam không đơn giản bởi vẻ đẹp tự nhiên của nó mà ở mỗi chiếc lá còn cất giữ trong tôi những câu chuyện đẹp thuở ấu thơ… Mảnh sân nhỏ trước nhà, nụ cười của bà, đôi bàn tay của mẹ, những giọt mưa mùa hạ, những tia nắng cuối thu… Qua kẽ lá, những giấc mơ trong tôi còn nguyên mãi…" - Tim Phạm</p>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -41,31 +42,7 @@ We enhance our customers living experience with the beauty of accessories; we ad
 	<?php include('./artworks.php'); ?>
 	<div class="section" id="section-details">
 		<div class="container">
-			<div class="row">
-				<!-- <div class="col-xs-12 col-md-6">
-					<h3>DIMENSIONS</h3>
-					<p>
-						<strong>Calendar</strong>
-						<br>
-						<span>20 cm x 20 cm</span>
-					</p>
-					<p>
-						<strong>Sketchbook</strong>
-						<br>
-						<span>14.8 cm x 22.3 cm</span>
-					</p>
-					<p>
-						<strong>Postcard</strong>
-						<br>
-						<span>12 cm x 17 cm</span>
-					</p>
-					<p>
-						<strong>Frames</strong>
-						50 cm x 70 cm<br>
-						40 cm x 60 cm<br>
-						16 cm x 22 cm
-					</p>
-				</div> -->
+			<div class="row animate">
 				<div class="col-xs-12 col-md-6">
 					<h3>KÍCH THƯỚC</h3>
 					<p>
@@ -91,66 +68,59 @@ We enhance our customers living experience with the beauty of accessories; we ad
 					16 cm x 22 cm
 					</p>
 				</div>
-				<!-- <div class="col-xs-12 col-md-6">
-					<h3>QUALITY</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				</div> -->
 			</div>
 		</div>
 	</div>
 	<div class="section" id="section-contact">
 		<div class="container nano">
 			<div class="row nano-content">
-				<div class="col-xs-12 col-md-6 text-left">
-					<div class="row">
-						<div class="col-xs-12">
-							<h3>SẢN PHẨM CÓ BÁN TẠI</h3>
-							<address>
-								<strong>Thinker & Dreamer Coffee</strong>
-								Lầu 3, 42 Nguyễn Huệ, Quận 1, Thành phố Hồ Chí Minh, Việt Nam<br>
-							</address>
-							<address>
-								<strong>Lam Boutique</strong>
-								<br>
-								98 Mạc Thị Bưởi, Quận 1, Thành phố Hồ Chí Minh, Việt Nam<br>
-							</address>
-							<address>
-								<strong>Nosbyn</strong>
-								<br>
-								77 Mạc Thị Bưởi, Quận 1, Thành phố Hồ Chí Minh, Việt Nam<br>
-							</address>
-							<address>
-								<strong>Ren</strong>
-								<br>
-								2A Nguyễn Thiệp, Quận 1, Thành phố Hồ Chí Minh, Việt Nam
-							</address>
-							<h3>FOLLOW US</h3>
-							<div><a class="link" href="http://facebook.com/smallerthanthreevn.vn">facebook.com/smallerthanthree.vn</a></div>
-							<div><a class="link" href="http://instagram.com/smallerthanthreevn.vn">instagram.com/smallerthanthree.vn</a></div>
+				<div class="animate">
+					<div class="col-xs-12 col-md-6 text-left">
+						<div class="row">
+							<div class="col-xs-12">
+								<h3>SẢN PHẨM CÓ BÁN TẠI</h3>
+								<address>
+									<strong>Thinker & Dreamer Coffee</strong>
+									Lầu 3, 42 Nguyễn Huệ, Quận 1, Thành phố Hồ Chí Minh, Việt Nam<br>
+								</address>
+								<address>
+									<strong>Lam Boutique</strong>
+									<br>
+									98 Mạc Thị Bưởi, Quận 1, Thành phố Hồ Chí Minh, Việt Nam<br>
+								</address>
+								<address>
+									<strong>Nosbyn</strong>
+									<br>
+									77 Mạc Thị Bưởi, Quận 1, Thành phố Hồ Chí Minh, Việt Nam<br>
+								</address>
+								<address>
+									<strong>Ren</strong>
+									<br>
+									2A Nguyễn Thiệp, Quận 1, Thành phố Hồ Chí Minh, Việt Nam
+								</address>
+								<h3>FOLLOW US</h3>
+								<div><a class="link" href="http://facebook.com/smallerthanthreevn.vn">facebook.com/smallerthanthree.vn</a></div>
+								<div><a class="link" href="http://instagram.com/smallerthanthreevn.vn">instagram.com/smallerthanthree.vn</a></div>
+							</div>
 						</div>
 					</div>
-				</div>
 
-				<div class="col-xs-12 col-md-6 text-left">
-					<h3>TELL US WHAT YOU THINK</h3>
-					<div class="alert alert-warning hidden" role="alert">Cảm ơn bạn đã liên hệ với chúng tôi!</div>
-					<form action="/" method="POST">
-						<div class="form-group">
-							<input type="text" name="name" class="form-control" id="inputName" placeholder="your name">
-						</div>
-						<div class="form-group">
-							<input type="email" name="email" class="form-control" id="inputEmail1" placeholder="your e-mail">
-						</div>
-						<div class="form-group">
-							<textarea class="form-control" name="content" id="inputContent" cols="5" rows="5" placeholder="your message"></textarea>
-						</div>
-						<button type="submit" class="btn btn-default text-bold pull-right btn-submit">SEND</button>
-					</form>
+					<div class="col-xs-12 col-md-6 text-left">
+						<h3>TELL US WHAT YOU THINK</h3>
+						<div class="alert alert-warning hidden" role="alert">Cảm ơn bạn đã liên hệ với chúng tôi!</div>
+						<form action="/" method="POST">
+							<div class="form-group">
+								<input type="text" name="name" class="form-control" id="inputName" placeholder="your name">
+							</div>
+							<div class="form-group">
+								<input type="email" name="email" class="form-control" id="inputEmail1" placeholder="your e-mail">
+							</div>
+							<div class="form-group">
+								<textarea class="form-control" name="content" id="inputContent" cols="5" rows="5" placeholder="your message"></textarea>
+							</div>
+							<button type="submit" class="btn btn-default text-bold pull-right btn-submit">SEND</button>
+						</form>
+					</div>
 				</div>
 			</div>
 		</div>
